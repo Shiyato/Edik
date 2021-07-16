864dee0365a7ed8574fe8ea75b08331ec9ece02c
Artemiy Zhdanov <a.z.v.20@mail.ru>
2021-07-16 21:09:29 +0300

    16.07 Update. Add a start and help messages.

--- edik.py
+++ edik.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
-import telebot
+from PIL import Image
+from time import sleep
+from telebot import TeleBot, types
 import os
 
 with open('config.txt', 'r') as file:
@@ -18,19 +20,56 @@ with open('config.txt', 'r') as file:
 
 engine = create_engine(f"mysql+pymysql://root:{db_pass}@{db_host}/{db_name}") #Connect to DB
 
-bot = telebot.TeleBot(token)
+bot = TeleBot(token)
+
+
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    pass #TODO add user in database
+def start(message):
+    bot.send_message(message.chat.id, "Привет, я - Эдик, бот, созданный чтобы помогать людям учиться (^_^)")
+    sleep(3)
+    bot.send_message(message.chat.id, "Может быть, у тебя есть любимое дело/хобби, которым тебе нравится заниматься? Или просто хочешь стать успешнее и учиться с намного большей эффективностью?")
+    sleep(5)
+    bot.send_message(message.chat.id, "Тогда, я помогу тебе.")
+    sleep(2)
+    help(message)
 
-@bot.message_handler(commands=['help', 'h', '?'])
-def send_welcome(message):
-    pass #TODO help message
+#photo = Image.open('lofi_girl.jpg')
+#bot.send_photo(message.chat.id, photo=photo, caption="")
+
+@bot.message_handler(commands=['help', 'h'])
+def help(message):
+    bot.send_message(message.chat.id, "Моя основная задача - это рассказать тебе о персональной модели обучения(ПМО).\n\n • Если хочешь узнать о ней, используй /edu или /e \n__________________________________________\n\nТакже я могу помочь с постановкой твоих личных целей и составлении планов (Все цели и планы сохраняются).\n\n • Чтобы начать ставить цели используй /aims или /a\n\n • Для планов используй /plans или /p\n__________________________________________\n\n • Вызвать список команд можно, используя /help или /h")
 
 @bot.message_handler(commands=['edu', 'e'])
-def send_welcome(message):
-    pass #TODO choise a educattion block
+def education(message):
+    bot.send_message(message.chat.id, "Итак, начнём")
+
+    markup = types.ReplyKeyboardMarkup()
+
+    yes = types.KeyboardButton('Есть')
+    no = types.KeyboardButton('Нет')
+
+    markup.row(yes, no)
+
+    sleep(1)
+    bot.send_message(message.chat.id, "Есть ли у тебя есть любимое дело, которому ты учишься или хотел бы научиться?", reply_markup=markup)
+
+"""@bot.callback_query_handler(func=lambda call: True)
+def handle(call):
+    answers = {
+        "yea1": "Отлично",
+        "no1": "Хорошо, тогда выбери его!"
+    }
+    bot.send_message(call.message.chat.id, answers.get(call.data))"""
+
+@bot.message_handler(commands=['aims', 'a'])
+def aims(message):
+    pass #TODO aims edit
+
+@bot.message_handler(commands=['plans', 'p'])
+def plans(message):
+    pass #TODO plans edit
 
 
 bot.polling(none_stop=True, interval=0) #Start the bot
