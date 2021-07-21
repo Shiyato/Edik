6bb8edc34ba92c2b185601d18e0a45962eb8e86e
Artemiy Zhdanov <a.z.v.20@mail.ru>
2021-07-21 21:16:13 +0300

    add quesions handler

--- edik.py
+++ edik.py
@@ -9,11 +9,10 @@ import os, db
 # Bot's message handlers
 bot = TeleBot(token) # Creating a bot object
 
-
-@bot.message_handler(commands=['start'])
-def quesion():
-    pass
-
+@bot.message_handler(func=lambda message: message.id - 1 == db.session.query(db.Support).filter(db.Support.user_id == message.user_from.id).first().last_quesion_id)
+def quesion(message):
+    bot.send_message(message.chat.id, "quesion")
+    bot.send_message(message.chat.id, "-- Извините, эта часть чат бота ещё в разработке (T_T) --") #TODO
 
 
 @bot.message_handler(commands=['start'])
