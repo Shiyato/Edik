a275dd6d8055243a8a8b51b26ad2a83fb647a033
Artemiy Zhdanov <a.z.v.20@mail.ru>
2021-07-21 19:04:23 +0300

    db connection fix

--- config_reader.py
+++ config_reader.py
@@ -2,7 +2,7 @@ import os
 token = os.environ['token']
 
 try:
-    db_url = os.environ['db_url']
+    db_url = os.environ['DATABASE_URL']
+    db_url = f'{db_url[:8]}+psycopg2{db_url[8:]}'
 except:
     raise ValueError(" Wrong database URL! ")
-
