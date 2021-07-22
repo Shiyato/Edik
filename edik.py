13e799ce3043c56bf32d4b559ab2dc7020a1b733
Artemiy Zhdanov <a.z.v.20@mail.ru>
2021-07-22 21:34:35 +0300

    Little fixes

--- edik.py
+++ edik.py
@@ -51,7 +51,7 @@ def delete_plan_point():
 
 # Aims functions
 
-def add_aim(user_id, text ):
+def add_aim(user_id, text):
     aim = db.Aims(user_id=user_id, aim_name=text)
     db.session.add(aim)
     db.session.commit()
@@ -197,7 +197,7 @@ def quesion(message):
     support = db.session.query(db.Support).filter(db.Support.user_id == user.id).first()
 
     if support.last_quesion_num == 'a1':
-        add_aim(message)
+        add_aim(user.id, message)
         bot.send_message(message.chat.id, "Цель сохранена! " + random.choice(["┏( ͡❛ ͜ʖ ͡❛)┛", "\( ͡❛ ͜ʖ ͡❛)/", "( ͡❛ ͜ʖ ͡❛)", "(>‿◠)✌"]))
 
 
