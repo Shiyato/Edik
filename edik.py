6cb3f84e81a06627ab3b2ea93d037fc8719e2477
Artemiy Zhdanov <a.z.v.20@mail.ru>
2021-07-23 20:58:04 +0300

    We can edit messages! :D

--- edik.py
+++ edik.py
@@ -11,6 +11,9 @@ bot = TeleBot(token) # Creating a bot object
 
 #TODO Add a check of database entry values
 
+def set_support(user_id, values:dict):
+    db.session.query(db.Support).filter(db.Support.user_id == user_id).update(values, synchronize_session='fetch')
+
 # Plans functions
 
 def add_plan(user_id, text):
@@ -18,10 +21,10 @@ def add_plan(user_id, text):
     db.session.add(plan)
     db.session.commit()
 
-def update_plan(plan_id, text):
+def edit_plan(plan_id, text): #FIXME
     db.session.query(db.Plans).filter(db.Plans.id == plan_id).update({"plan_name" == text}, synchronize_session='fetch')
 
-def delete_plan(plan_id):
+def delete_plan(plan_id): #FIXME
     plan = db.session.query(db.Plans).filter(db.Plans.id == plan_id).first()
     db.session.delete(plan)
     db.session.commit()
@@ -42,7 +45,7 @@ def add_plan_point(plan_id, number):
     db.session.add(plan_point)
     db.session.commit()
 
-def update_plan_point():
+def edit_plan_point():
     pass #TODO
 
 def delete_plan_point():
@@ -51,19 +54,25 @@ def delete_plan_point():
 
 # Aims functions
 
-def add_aim(user_id, text):
-    aim = db.Aims(user_id=user_id, aim_name=text)
-    db.session.add(aim)
-    db.session.commit()
+def add_aim(chat_id, user_id, text):
+    if db.session.query(db.Aims).filter(db.Aims.user_id == user_id and db.Aims.aim_name == text):
+        bot.send_message(chat_id, "Прости, но целям нельзя давать одинаковые имена")
+    else:
+        aim = db.Aims(user_id=user_id, aim_name=text)
+        db.session.add(aim)
+        db.session.commit()
 
-def update_aim():
-    pass #TODO
+def edit_aim(aim_q, text):
+    aim_q.update({"aim_name": text}, synchronize_session='fetch')
 
 def delete_aim():
     pass #TODO
 
 def complete_aim():
     pass #TODO
+
+def choise_aim(user_id, text):
+    return db.session.query(db.Aims).filter(db.Aims.user_id == user_id and db.Aims.aim_name == text)
 
 
 # Find user function
@@ -155,29 +164,33 @@ def aims(message):
 @bot.message_handler(commands=['add_aim', 'aa'])
 def add_aim_h(message):
     user = find_user(message.from_user.id)
-    support = db.session.query(db.Support).filter(db.Support.user_id == user.id).update({"last_quesion_id": message.message_id + 1, "last_quesion_num": "a1"}, synchronize_session='fetch')
+    set_support(user.id, {"last_quesion_id": message.message_id + 1, "last_quesion_num": "a1"})
     bot.send_message(message.chat.id, "Введи название цели:")
 
 
 @bot.message_handler(commands=['edit_aim', 'ea'])
 def edit_aim_h(message):
     user = find_user(message.from_user.id)
-    support = db.session.query(db.Support).filter(db.Support.user_id == user.id).update({"last_quesion_id": message.message_id + 1, "last_quesion_num": "a2"}, synchronize_session='fetch')
-    bot.send_message(message.chat.id, "Выбери цель:")
-    bot.send_message(message.chat.id, "-- Извините, эта часть чат бота ещё в разработке (T_T) --") #TODO
+    aims = db.session.query(db.Aims).filter(db.Aims.user_id == user.id).all()
+    set_support(user.id, {"last_quesion_id": message.message_id + 1, "last_quesion_num": "a2"})
+
+    markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True, selective=True)
+    for aim in aims:
+        aim_name = types.KeyboardButton(aim.aim_name)
+        markup.add(aim_name)
+    bot.send_message(message.chat.id, "Выбери цель:", reply_markup=markup)
 
 @bot.message_handler(commands=['delete_aim', 'da'])
 def delete_aim_h(message):
     user = find_user(message.from_user.id)
-    support = db.session.query(db.Support).filter(db.Support.user_id == user.id).update({"last_quesion_id": message.message_id + 1, "last_quesion_num": "a3"}, synchronize_session='fetch')
+    set_support(user.id, {"last_quesion_id": message.message_id + 1, "last_quesion_num": "a3"})
     bot.send_message(message.chat.id, "Выбери цель:")
-    bot.send_message(message.chat.id, "-- Извините, эта часть чат бота ещё в разработке (T_T) --") #TODO
 
 
 
 @bot.message_handler(commands=['plans', 'p'])
 def plans(message):
-    plans_list = db.session.query(db.Plans).filter(db.Plans.user_id == find_user(message.from_user.id).id)
+    plans_list = db.session.query(db.Plans).filter(db.Plans.user_id == find_user(message.from_user.id).id).all()
     bot.send_message(message.chat.id, "Вот список твоих планов:\n")
     bot.send_message(message.chat.id, "-- Извините, эта часть чат бота ещё в разработке (T_T) --") #TODO
 
@@ -194,13 +207,27 @@ def dev(message):
 @bot.message_handler(func=que_handler)
 def quesion(message):
     user = find_user(message.from_user.id)
+    aim_q = choise_aim(user, message.text)
+    rand_smile = random.choice(["┏( ͡❛ ͜ʖ ͡❛)┛", "┌( ಠ‿ಠ)┘", "\( ͡❛ ͜ʖ ͡❛)/", "\(•◡•)/", "( ͡❛ ͜ʖ ͡❛)", "(>‿◠)✌", "ʕ•ᴥ•ʔ", "(◉◡◉)", "(◕‿◕)", "( ́ ◕◞ε◟◕`)", "(^ↀᴥↀ^)"])
     support = db.session.query(db.Support).filter(db.Support.user_id == user.id).first()
 
     if support.last_quesion_num == 'a1':
-        add_aim(user.id, message.text)
-        bot.send_message(message.chat.id, "Цель сохранена! " + random.choice(["┏( ͡❛ ͜ʖ ͡❛)┛", "\( ͡❛ ͜ʖ ͡❛)/", "( ͡❛ ͜ʖ ͡❛)", "(>‿◠)✌"]))
+        add_aim(message.chat.id, user.id, message.text)
+        bot.send_message(message.chat.id, "Цель сохранена! " + rand_smile)
 
+    if support.last_quesion_num == 'a2':
+        set_support(user.id, {"last_quesion_id": message.message_id + 1, "last_quesion_num": "a2_1"})
+        bot.send_message(message.chat.id, "Введи название:")
 
+    if support.last_quesion_num == 'a2_1':
+        edit_aim(aim_q, message.text)
+        bot.send_message(message.chat.id, "Цель изменена! " + rand_smile)
+
+    if support.last_quesion_num == 'a3':
+        pass
+
+    
+    
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0) #Starting the bot
